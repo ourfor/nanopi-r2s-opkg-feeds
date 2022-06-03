b2fcf13f6fbfff50852c72e12eaa5157a0db9794
Sagit <ourfor@foxmail.com>
2022-06-03 14:05:58 +0000

    💚 update build script

--- init_env.sh
+++ init_env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo rm -rf /etc/apt/sources.list.d
-sudo apt-get update
 sudo apt-get -y --no-install-recommends install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler antlr3 jq
-curl https://raw.githubusercontent.com/friendlyarm/build-env-on-ubuntu-bionic/master/install.sh  | sed '/#/d' | sed 's/\\//g' | sed 's/exit 0//g' | sed 's/sudo apt -y install//g' | sed 's/sudo apt-get -y install//g' | sed 's/:i386//g' | xargs sudo apt-get -y --no-install-recommends install
-sudo rm -rf /usr/share/dotnet /usr/local/lib/android/sdk
-sudo docker image prune -a -f
+sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
+sudo apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove --purge
+sudo apt-get clean
+wget -O - https://raw.githubusercontent.com/friendlyarm/build-env-on-ubuntu-bionic/master/install.sh | bash
 
 git clone https://github.com/friendlyarm/repo
 sudo cp repo/repo /usr/bin/
