475d554456258b93bac5afc3c3ac43ea6e138978
Sagit <ourfor@foxmail.com>
2022-06-04 01:02:33 +0000

    💚 update openwrt build script

---
+++ add_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd src/openwrt
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git pull
+./scripts/feeds update -a
+./scripts/feeds install -a
+cat feeds.conf.default
---
+++ pull_source.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir src
+cd src
+git clone -b openwrt-22.03 https://github.com/openwrt/openwrt.git openwrt
